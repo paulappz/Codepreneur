c2a4fefd3f7f3cfe804cf2a84c90bbb4802993e1
RainbowSeven <thezurg07@gmail.com>
2016-11-02 12:23:57 +0100

    update auth routes

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,9 @@
 <?php
 
 Route::get('/', 'RootController@index');
-Route::post('/auth', 'AuthController@post');
+Route::post('/auth/login', 'AuthController@store');
+Route::delete('/auth/logout', 'AuthController@destroy');
+
+Route::resource('user', 'UserController', [
+    'only' => ['store', ]
+]);
