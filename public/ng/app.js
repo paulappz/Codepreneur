637996a074e9257dab107916e02b211aadd87e77
RainbowSeven <thezurg07@gmail.com>
2016-11-02 01:00:54 +0100

    move templates around

---
+++ public/ng/app.js
@@ -0,0 +1,44 @@
+angular.module('WakaOndo', ['ngRoute',])
+
+.config(function($routeProvider, $interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+  
+  $routeProvider.when('/', {
+      controller:'IndexController as index',
+      templateUrl:'/ng/templates/slide.html',
+    })
+    .when('/login', {
+      controller:'LoginController as login',
+      templateUrl:'/ng/templates/login.html',
+    })
+    .when('/signup', {
+      controller:'SignupController as signup',
+      templateUrl:'/ng/templates/signup.html',
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+
+.controller('IndexController', function() {
+
+})
+.controller('LoginController', function() {
+
+})
+.controller('NavigationController', ['$scope', '$location', function($scope, $location) {
+  $scope.items = [
+    {path: '/', title: 'Home'},
+    {path: '/about', title: 'About'},
+    {path: '/contact', title: 'Contact'},
+    {path: '/login', title: 'Login'},
+    {path: '/signup', title: 'Signup'},
+  ];
+  $scope.isActive = function(item) {
+    if (item.path == $location.path()) {
+      return true;
+    }
+    return false;
+  }
+}]);
