f840714c9b877b7d0e9c99c90fabbaac571c3ed5
RainbowSeven <thezurg07@gmail.com>
2016-11-07 01:09:12 +0100

    update routes

--- app/routes.php
+++ app/routes.php
@@ -7,3 +7,13 @@ Route::delete('/auth/logout', 'AuthController@destroy');
 Route::resource('user', 'UserController', [
     'only' => ['store', ]
 ]);
+
+Route::group(['prefix'=>'api/v1'], function() {
+    Route::resource('places', 'PlaceController', [
+     'only' => ['index', 'show']
+    ]);
+
+    Route::resource('areas', 'AreaController', [
+     'only' => ['index', 'show']
+    ]);   
+});
